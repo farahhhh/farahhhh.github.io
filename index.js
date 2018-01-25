7066b12da96c5296ac7bc31804603b484316d462
farah <farahillianafa@gmail.com>
2018-01-25 15:31:34 +0800

    v1.0

---
+++ index.js
@@ -0,0 +1,87 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    $(".login-cover").hide();
+
+     var dialog = document.querySelector('#loginDialog');
+
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.close();
+     console.log("login");
+    window.location.replace("homepage.html");
+
+  } else {
+    // No user is signed in.
+    $(".login-cover").show();
+
+    var dialog = document.querySelector('#loginDialog');
+
+    if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.showModal();
+    console.log("logout");
+  }
+});
+
+
+
+$("#loginBtn").click(
+	function(){
+
+		var email = $("#loginEmail").val();
+		var password = $("#loginPassword").val();
+
+		if(email < 6 ){
+			alert("Tolong masukkan email");
+		}
+		else if(password <4){
+			alert("Tolong masukkan kata laluan");
+		}
+
+		if(email !="" && password != ""){
+
+			$("#loginProgress").show();
+			$("#loginBtn").hide();
+
+			firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
+
+			$("#loginError").show().text(error.message);
+
+			$("#loginProgress").hide();
+			$("#loginBtn").show();
+
+			});
+
+
+		}
+		
+	}
+);
+
+
+/* LOGOUT PROCESS */
+$("#signOutBtn").click(
+	function(){
+
+		firebase.auth().signOut().then(function() {
+
+		}).catch(function(error) {
+		  // An error happened.	
+		  alert(error.message);
+
+		});
+
+			$("#loginProgress").hide();
+			$("#loginBtn").show();
+			localStorage.setItem("totalCart",0);
+			localStorage.setItem("totalBil",0);
+						window.location.replace("index.html");	
+	});
+
+
+
+/*COMMENT END*/
